aafb938f896cfba21e2954584b8cab7aeeabbcaa
Jordan Smith <jordan.smith.1249@gmail.com>
2025-02-13 18:05:35 -0500

    New test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-#include <SDL.h>
-int main() {
+#include <SDL2/SDL.h>
+#define SDL_FLAGS 0
+int main(int argc, char * argv[]) {
+	if (SDL_Init(SDL_FLAGS)) {
+		std::cout << SDL_GetError();
+		return 1;
+	}
+	SDL_Window * window = SDL_CreateWindow("Hello world",
+		SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
+		640,480,0);
 	std::cout << "Hello world\n";
+	SDL_Delay(2000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
